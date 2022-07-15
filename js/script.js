fbbaa1e4bb2232f421dc3a5c585cfacce6ae6e02
chazarreta cristian gabriel <cristianchazarreta2021@gmail.com>
2022-07-15 15:18:24 -0300

    arreglo de input

--- js/script.js
+++ js/script.js
@@ -5,17 +5,13 @@ class Usuarios{
         this.textArea=textArea
     }
 }
-
 const user = []
-
 const form = document.getElementById("formulario")
-
 form.addEventListener("submit",(evento)=>{
     evento.preventDefault()
-
-    let nombre=document.getElementById("nombre")
-    let correo=document.getElementById("correo")
-    let textArea = document.getElementById("textArea")
+    let nombre=document.getElementById("nombre").value
+    let correo=document.getElementById("correo").value
+    let textArea = document.getElementById("textArea").value
     const usuario = new Usuarios(nombre,correo,textArea)
     user.push(usuario)
     console.log(user)
